9ed05270be56616c5bb50739009b5c02f798b938
박지수 <jisupark2000@gmail.com>
2023-07-05 20:42:06 +0900

    프로젝트 초기 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const px0_10 = { ...Array.from(Array(11)).map((_, i) => `${i}px`) };
+const px0_100 = { ...Array.from(Array(101)).map((_, i) => `${i}px`) };
+const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
+const px0_1000 = { ...Array.from(Array(1001)).map((_, i) => `${i}px`) };
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      borderWidth: px0_10,
+      // fontSize: px0_100, // fontsize를 rem 대신 px로 할거면 주석 해제
+      lineHeight: px0_100,
+      width: px0_1000,
+      height: px0_1000,
+      spacing: px0_200,
+      borderRadius: px0_100,
+    },
+  },
+  plugins: [],
+};
