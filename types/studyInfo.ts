c54d4c4be2c630dab4f7fe109f5b05d956ccdc37
박지수 <jisupark2000@gmail.com>
2023-08-06 21:45:26 +0900

    메인 페이지 스터디 받아오기 api

---
+++ types/studyInfo.ts
@@ -0,0 +1,13 @@
+export type StudyCategory = 'ALL' | '프로그래밍' | '언어' | '취업' | '고시/공무원' | '취미/교양' | '기타';
+export const studyCategories: StudyCategory[] = [
+  'ALL',
+  '프로그래밍',
+  '언어',
+  '취업',
+  '고시/공무원',
+  '취미/교양',
+  '기타',
+];
+
+export type StudyChannel = '온라인' | '오프라인' | '병행';
+export type StudyCampus = '인문' | '자연' | '공통';
---
+++ types/studyParams.ts
@@ -0,0 +1,7 @@
+export type StudySortingMethod = 'recent' | 'recruit_popular' | 'recruit_deadline';
+
+export const studySortingMethodMapping = {
+  recent: '최신순',
+  recruit_popular: '인기순',
+  recruit_deadline: '마감임박순',
+};
