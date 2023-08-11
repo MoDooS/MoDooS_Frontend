34f0ce386f88b5253cdb937fe19fe50ec9e15a67
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-08-11 14:48:14 +0900

    feat: 피드백 페이지

---
+++ components/feedback/title.tsx
@@ -0,0 +1,14 @@
+interface Prop {
+  text: string;
+}
+
+const Title: React.FC<Prop> = ({ text }) => {
+  return (
+    <>
+      <h1 className=' text-black font-semibold text-25 mb-20'>{text}</h1>
+      <div className='bg-gray_50 w-full h-1'></div>
+    </>
+  );
+};
+
+export default Title;
