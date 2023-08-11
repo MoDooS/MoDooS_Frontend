7a17b11ddbb735955593d17ff1ae945f4865f6d1
박지수 <jisupark2000@gmail.com>
2023-08-11 14:50:04 +0900

    Feat react-query 기본값 설정 -> retry-0 refetchOnWindowFocus-false

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,9 @@ import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   // next.js는 페이지 전환마다 _app.tsx가 렌더링되므로 queryClient가 재선언되지 않게 useState로 관리
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () => new QueryClient({ defaultOptions: { queries: { retry: 0, refetchOnWindowFocus: false } } }),
+  );
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
