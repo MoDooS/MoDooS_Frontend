55cebf7ec160cc4ed351bd502f33d9e0b6c8d487
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-08-16 17:33:20 +0900

    feat: 스터디 상세 날짜 추가

--- components/feedback/title.tsx
+++ components/feedback/title.tsx
@@ -1,12 +1,19 @@
 interface Prop {
   text: string;
+  startAt?: string;
+  endAt?: string;
 }
 
-const Title: React.FC<Prop> = ({ text }) => {
+const Title: React.FC<Prop> = ({ text, startAt, endAt }) => {
   return (
     <>
-      <h1 className=' text-black font-semibold text-25 mb-20'>{text}</h1>
-      <div className='bg-gray_30 w-full h-1'></div>
+      <h1 className=' text-black font-semibold text-25'>{text}</h1>
+      {startAt && (
+        <div className='mt-15 text-13 text-gray_70'>
+          시작일: {startAt}&nbsp;&nbsp;&nbsp;종료일:{endAt}
+        </div>
+      )}
+      <div className='bg-gray_30 w-full h-1 mt-20'></div>
     </>
   );
 };
