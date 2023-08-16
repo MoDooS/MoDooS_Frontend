333171906377f37ba30eb757c46a9f3653134ec3
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-08-16 17:34:42 +0900

    style: 상세규칙 체크박스 추가

--- components/feedback/checklist.tsx
+++ components/feedback/checklist.tsx
@@ -1,15 +1,23 @@
+declare module 'react' {
+  interface CSSProperties {
+    WebkitAppearance?: string;
+    MozAppearance?: string;
+  }
+}
+
 const CheckList = () => {
+  const customCheckboxStyle: React.CSSProperties = {
+    WebkitAppearance: 'auto',
+    MozAppearance: 'auto',
+    appearance: 'auto',
+  };
+
+  const checkboxStyle = {};
   return (
     <div className='flex flex-col mt-26 mb-108 gap-15'>
-      <div className='items-center bg-gray_40 w-full px-13 py-11 rounded-5 border border-gray_30'>
-        <input type='checkbox' id='coding' name='interest' value='coding' checked />
-        <label className='text-14'>과제를 성실히 해오기 </label>
-      </div>
-      <div className='items-center bg-gray_40 w-full px-13 py-11 border border-gray_30'>
-        <input type='checkbox' checked /> <label className='text-14'>과제를 성실히 해오기 </label>
-      </div>
-      <div className='items-center bg-gray_40 w-full px-13 py-11 border border-gray_30'>
-        <input type='checkbox' checked /> <label className='text-14'>과제를 성실히 해오기 </label>
+      <div className='items-center bg-gray_40 w-full px-13 py-11 border border-gray_30 flex justify-between'>
+        <label className='text-14'>과제를 성실히 해오기 </label>{' '}
+        <input style={customCheckboxStyle} className='inline w-17 h-17 bg-white' type='checkbox' checked />{' '}
       </div>
       <div className='items-center bg-gray_40 w-full px-13 py-11 border border-gray_30'>
         <input type='checkbox' checked /> <label className='text-14'>과제를 성실히 해오기 </label>
