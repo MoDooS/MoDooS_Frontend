c0832ad0d489d015fd184c0a1b575982fa52ddb1
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-08-23 02:53:53 +0900

    feat: 유저 정보 페이지 UI

--- components/settingInfo/settingInfo.tsx
+++ components/settingInfo/settingInfo.tsx
@@ -1,10 +1,24 @@
+import { SettingInfoProps } from '@/pages/mypage/setting';
+import { MyInfoRequest } from '@/types/myInfoRequest';
 import React from 'react';
+import Category from '../auth/category';
+import { StudyCampus } from '@/types/studyInfo';
 
-type SettingInfoProps = {
-  isEditing: boolean;
-};
+const SettingInfo: React.FC<SettingInfoProps> = ({ isEditing, editedUser, updateEditedUser, user }) => {
+  // 각 입력 필드에서 값이 변경될 때 호출되는 함수
+  const handleFieldChange = (event: React.ChangeEvent<HTMLInputElement>, value: string, field: keyof MyInfoRequest) => {
+    updateEditedUser((draft) => {
+      draft[field] = event.target.value;
+      return draft;
+    });
+  };
 
-const SettingInfo: React.FC<SettingInfoProps> = ({ isEditing }) => {
+  const handleCategoryChange = (campus: StudyCampus, major: string) => {
+    updateEditedUser((draft) => {
+      draft.campus = campus;
+      draft.department = major;
+    });
+  };
   return (
     <div className='mb-50 flex flex-col gap-10'>
       {isEditing ? (
@@ -12,45 +26,41 @@ const SettingInfo: React.FC<SettingInfoProps> = ({ isEditing }) => {
           <div className='text-15'>
             {' '}
             닉네임:
-            <input type='text' defaultValue={'하이'} className='ml-8 p-2 pl-4 border border-gray_60 rounded-12' />
-          </div>
-          <div className='text-15'>
-            {' '}
-            이메일:
             <input
               type='text'
-              defaultValue='example@mju.ac.kr'
-              className='ml-8 p-2 pl-4 border border-gray_60 rounded-12'
+              defaultValue={user?.nickname}
+              onChange={(event) => handleFieldChange(event, event.target.value, 'nickname')}
+              className='ml-8 py-8 px-10 rounded-17 border border-gray_70'
             />
           </div>
           <div className='text-15'>
             {' '}
-            캠퍼스:
-            <input type='text' defaultValue='인문' className='ml-8 p-2 pl-4 border border-gray_60 rounded-12' />
+            이메일:
+            <span>&nbsp;&nbsp; {user?.email}</span>
           </div>
           <div className='text-15'>
             {' '}
-            학과:&nbsp;&nbsp;&nbsp;
-            <input
-              type='text'
-              defaultValue='융합소프트웨어학부'
-              className='ml-8 p-2 pl-4 border border-gray_60 rounded-12'
+            캠퍼스:&nbsp;&nbsp;
+            <Category
+              onChange={handleCategoryChange}
+              defaultCampus={user?.campus}
+              defaultDepartment={user?.department}
             />
           </div>
         </div>
       ) : (
         <div className='flex flex-col gap-13'>
           <div className='text-15'>
-            닉네임 <span>: 하이</span>
+            닉네임 <span>: {user?.nickname}</span>
           </div>
           <div className='text-15'>
-            이메일 <span>: example@mju.ac.kr</span>
+            이메일 <span>: {user?.email}</span>
           </div>
           <div className='text-15'>
-            캠퍼스 <span>: 인문</span>
+            캠퍼스 <span>: {user?.campus}</span>
           </div>
           <div className='text-15'>
-            학과 <span>: 융합소프트웨어학부</span>
+            학과 <span>: {user?.department}</span>
           </div>
         </div>
       )}
