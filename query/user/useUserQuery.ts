657250e1e2890497deac2c696e0294c8c6ba56d3
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-08-23 02:54:45 +0900

    feat: 현재 유저정보 확인 로그

--- query/user/useUserQuery.ts
+++ query/user/useUserQuery.ts
@@ -1,10 +1,6 @@
 import { getUser } from '@/apis/getUser';
-import modoosAxios from '@/apis/modoosAxios';
 import { withVerify } from '@/apis/withVerify';
 import { authToken } from '@/class/authToken';
-import { CreditRating } from '@/lib/creditRating';
-import { StudyCampus } from '@/types/studyInfo';
-import { AxiosError, AxiosResponse } from 'axios';
 import { useQuery } from 'react-query';
 
 export const USER_QUERY_KEY = 'userQuery';
@@ -19,8 +15,12 @@ export function useUserQuery() {
     queryKey: USER_QUERY_KEY,
     queryFn: () => withVerify(() => getUser(accessToken)),
     select: (res) => res.data,
+    onSuccess: (newData) => {
+      console.log('Received updated user data:', newData); // 업데이트된 유저정보 확인용
+    },
     onError: () => {}, // useUser에서의 401에러는 공통 에러 처리 로직을 따르지 않는다.
     staleTime: 1000 * 20, // 20초 동안 유효한 데이터
   });
+
   return { user, isLoading, isError };
 }
